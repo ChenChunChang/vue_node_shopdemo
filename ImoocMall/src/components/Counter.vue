a7fe86a1ea7f4fcd875005d24ac69715a0a423ae
chenchunchang <13619444487@qq.com>
2017-11-17 13:56:45 +0800

    "第二章关于父子之间的传值问题的测试文件"

---
+++ ImoocMall/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+    <p><span>{{num}}</span></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['num'],
+    data(){
+      return{
+//        num:0,
+//        msg:"hello vue!"
+      }
+    },
+    methods:{
+      increase(){
+        this.$emit("incre");
+      },
+      decrease(){
+        this.$emit("decre");
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
