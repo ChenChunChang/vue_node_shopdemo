8ffdbd842a5cce2dd773849d36a45d5199558c23
ChenChunChang <1361944487@qq.com>
2017-11-19 20:03:40 +0800

    "mymall"

---
+++ MyMall/src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+    <p><span>{{num}}</span></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['num'],
+    data(){
+      return{
+//        num:0,
+//        msg:"hello vue!"
+      }
+    },
+    methods:{
+      increase(){
+        this.$emit("incre");
+
+        import('./../util');
+      },
+      decrease(){
+        this.$emit("decre");
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
